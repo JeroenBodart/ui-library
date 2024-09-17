9eedffa58c0c75980ea30d495842b684e8eb72da
Jeroen Bodart <jeroen.bodart@belgianrail.com>
2024-09-17 12:41:27 +0200

    testing ui library to project connection

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
       formats: ["es"]
     },
     rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
+      external: ["vue"],
       input: Object.fromEntries(
         // https://rollupjs.org/configuration-options/#input
         glob.sync("lib/**/*.{ts,tsx}", {
